add6e99c4367e81d803bf068c9f35ad7b9c7cdc3
Csikvár Tamás <csikvar.tamas@gmail.com>
2023-06-10 19:43:43 +0200

    ötödik feladat kész

--- models/car_position.py
+++ models/car_position.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from sqlalchemy.sql.functions import now
 from db import db
+import requests
 
 class PositionModel(db.Model):
     __tablename__ = 'positions'
@@ -10,12 +11,28 @@ class PositionModel(db.Model):
     car = db.relationship('CarModel', backref='positions')
     latitude = db.Column(db.Float(precision=5))
     longitude = db.Column(db.Float(precision=5))
+    address = db.Column(db.String(300))  # új oszlop
     date = db.Column(db.DateTime, default=now)
+
+    @staticmethod
+    def resolve_address(latitude, longitude):
+        response = requests.get('https://nominatim.openstreetmap.org/reverse', params={
+            'lat': latitude,
+            'lon': longitude,
+            'format': 'json'
+        })
+
+        if response.status_code != 200:
+            return ''
+
+        data = response.json()
+        return data.get('display_name', '')
 
     def __init__(self, car_id, latitude, longitude):
         self.car_id = car_id
         self.latitude = latitude
         self.longitude = longitude
+        self.address = self.resolve_address(latitude, longitude)
         self.date = datetime.utcnow()
 
     def save_to_db(self):
@@ -28,5 +45,6 @@ class PositionModel(db.Model):
             'car_id': self.car_id,
             'latitude': self.latitude,
             'longitude': self.longitude,
+            'address': self.address,  # include in the json response
             'date': self.date.isoformat()
         }
